c87ec16abf66e5926d12d9888be84449945ca2e6
headbang <irockezio@gmail.com>
2012-01-16 18:16:13 +0400

    Realise reading configuration from INI-file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <boost/property_tree/ini_parser.hpp>
 
 int main()
 {
+    boost::property_tree::ptree config;
+    try
+    {
+        boost::property_tree::read_ini("/etc/shrimp.ini", config);
+    }
+    catch (boost::property_tree::ini_parser_error error)
+    {
+        std::cout
+                << error.message() << ":"
+                << error.filename() << ", line"
+                << error.line() << std::endl;
+        exit(1);
+    }
     return 0;
 }
 
