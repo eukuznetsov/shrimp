37e141811b8f87576c078046c715b6f57ff86c50
headbang <irockezio@gmail.com>
2012-01-22 14:52:16 +0400

    Add class BaseDatabase

---
+++ basedatabase.cpp
@@ -0,0 +1,5 @@
+#include "basedatabase.h"
+
+BaseDatabase::BaseDatabase()
+{
+}
---
+++ basedatabase.h
@@ -0,0 +1,10 @@
+#ifndef BASEDATABASE_H
+#define BASEDATABASE_H
+
+class BaseDatabase
+{
+public:
+    BaseDatabase();
+};
+
+#endif // BASEDATABASE_H
