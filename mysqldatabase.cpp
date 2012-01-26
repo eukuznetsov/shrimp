2a13c6e87caa730c8f37c775cac7cae81e25d5ef
headbang <irockezio@gmail.com>
2012-01-27 00:39:08 +0400

    Write constructor and destructor of class MysqlDatabase.

--- mysqldatabase.cpp
+++ mysqldatabase.cpp
@@ -1,5 +1,28 @@
 #include "mysqldatabase.h"
+#include "databaseexcp.h"
+#include <iostream>
+#include <string>
+#include <mysql.h>
+#include <my_global.h>
 
-MysqlDatabase::MysqlDatabase()
+MysqlDatabase::MysqlDatabase(const char* host,
+                             const char* user,
+                             const char* password,
+                             const char* database,
+                             int port,
+                             const char* unix_socket,
+                             unsigned long clientflag)
 {
+    if(mysql_init(&dbsession)==NULL)
+    {
+        mysql_close(&dbsession);
+        throw DatabaseError("Connection to mysql database creating failed");
+    }
+    if(mysql_real_connect(&dbsession, host, user, password,
+                          database, port, unix_socket, clientflag) == NULL)
+    {
+        std::string err(mysql_error(&dbsession));
+        mysql_close(&dbsession);
+        throw DatabaseError(err.c_str());
+    }
 }
--- mysqldatabase.h
+++ mysqldatabase.h
@@ -1,11 +1,17 @@
 #ifndef MYSQLDATABASE_H
 #define MYSQLDATABASE_H
 
+#include <mysql.h>
+
 class MysqlDatabase
 {
   MYSQL dbsession;
 public:
-  MysqlDatabase();
+  MysqlDatabase(const char*, const char*, const char*, const char*,
+                int port=0,
+                const char* unix_socket=0,
+                unsigned long clientflag=0);
+  ~MysqlDatabase() { mysql_close(&dbsession); }
 };
 
 #endif // MYSQLDATABASE_H
