1671fe6ef35af4439f3239261c0b9eb6050b9835
nikita bukhvostov <dragon.djanic@gmail.com>
2012-04-14 19:46:56 +0400

    db-init.sh >> add echo comment message for passwd

--- scripts/db-init.sh
+++ scripts/db-init.sh
@@ -6,4 +6,5 @@ DATABASE="shrimp"
 HOST="localhost"
 PASSWORD="test"
 
+echo "Enter MySQL root passwd"
 $MYSQL -u root -p -e "CREATE DATABASE IF NOT EXISTS ${DATABASE}; GRANT ALL ON *.* TO '${DATABASE}'@'${HOST}' IDENTIFIED BY '${PASSWORD}';FLUSH PRIVILEGES;"
